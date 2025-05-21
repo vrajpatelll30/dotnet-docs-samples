ba4cdc13c471962071fdca683449ac7f261c1bde
vraj patel2 <vraj.patel2@crestdata.ai>
2025-05-21 17:22:19 +0530

    changes in progress

--- modelarmor/api/ModelArmor.Samples.Tests/ModelArmorFixture.cs
+++ modelarmor/api/ModelArmor.Samples.Tests/ModelArmorFixture.cs
@@ -1,18 +1,22 @@
-// Copyright(c) 2024 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not
-// use this file except in compliance with the License. You may obtain a copy of
-// the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations under
-// the License.
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 using System;
+using System.Collections.Generic;
+using Google.Api.Gax.ResourceNames;
 using Google.Cloud.ModelArmor.V1;
 using Xunit;
 
@@ -22,29 +26,32 @@ public class ModelArmorFixture : IDisposable, ICollectionFixture<ModelArmorFixtu
     public ModelArmorClient Client { get; }
     public string ProjectId { get; }
     public string LocationId { get; }
+    public string TemplateId { get; }
     public TemplateName TemplateForQuickstartName { get; }
+
+    public string InspectTemplateId => "dlp-inspect-template-1";
+    public string DeidentifyTemplateId => "dlp-deidentify-template-1";
 
     public ModelArmorFixture()
     {
         // Get the Google Cloud ProjectId
         ProjectId = Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
-        if (string.IsNullOrEmpty(ProjectId))
+        if (String.IsNullOrEmpty(ProjectId))
         {
-            throw new Exception("Missing GOOGLE_PROJECT_ID environment variable");
+            throw new Exception("missing GOOGLE_PROJECT_ID");
         }
 
-        // Get location ID from environment variable or use default
-        LocationId = Environment.GetEnvironmentVariable("GOOGLE_CLOUD_LOCATION") ?? "us-central1";
+        // Get LocationId (e.g., "us-west1")
+        LocationId = Environment.GetEnvironmentVariable("GOOGLE_CLOUD_LOCATION") ?? "us-west1";
 
-        // Create client
+        // Create the Model Armor Client
         Client = ModelArmorClient.Create();
 
-        // Create a template name for quickstart test
-        string templateId = $"test-template-{Guid.NewGuid().ToString("N").Substring(0, 8)}";
+        TemplateId = $"test-template-{Guid.NewGuid().ToString("N").Substring(0, 8)}";
         TemplateForQuickstartName = TemplateName.FromProjectLocationTemplate(
             ProjectId,
             LocationId,
-            templateId
+            TemplateId
         );
     }
 
@@ -63,8 +70,58 @@ public class ModelArmorFixture : IDisposable, ICollectionFixture<ModelArmorFixtu
         }
     }
 
-    public string RandomId()
+    public Template CreateTemplateConfiguration()
     {
-        return $"csharp-{Guid.NewGuid()}";
+        RaiFilterSettings raiFilterSettings = new RaiFilterSettings();
+        raiFilterSettings.RaiFilters.Add(
+            new RaiFilterSettings.Types.RaiFilter
+            {
+                FilterType = RaiFilterType.Dangerous,
+                ConfidenceLevel = DetectionConfidenceLevel.High,
+            }
+        );
+        raiFilterSettings.RaiFilters.Add(
+            new RaiFilterSettings.Types.RaiFilter
+            {
+                FilterType = RaiFilterType.HateSpeech,
+                ConfidenceLevel = DetectionConfidenceLevel.MediumAndAbove,
+            }
+        );
+        raiFilterSettings.RaiFilters.Add(
+            new RaiFilterSettings.Types.RaiFilter
+            {
+                FilterType = RaiFilterType.SexuallyExplicit,
+                ConfidenceLevel = DetectionConfidenceLevel.MediumAndAbove,
+            }
+        );
+        raiFilterSettings.RaiFilters.Add(
+            new RaiFilterSettings.Types.RaiFilter
+            {
+                FilterType = RaiFilterType.Harassment,
+                ConfidenceLevel = DetectionConfidenceLevel.MediumAndAbove,
+            }
+        );
+
+        // Create the filter config with RAI settings
+        FilterConfig modelArmorFilter = new FilterConfig { RaiSettings = raiFilterSettings };
+
+        // Create base template configuration
+        Template template = new Template { FilterConfig = modelArmorFilter };
+
+        return template;
+    }
+
+    public string CreateTemplate()
+    {
+        Template template = CreateTemplateConfiguration();
+        CreateTemplateRequest request = new CreateTemplateRequest
+        {
+            ParentAsLocationName = LocationName.FromProjectLocation(ProjectId, LocationId),
+            TemplateId = TemplateId,
+            Template = template,
+        };
+        Template createdTemplate = Client.CreateTemplate(request);
+
+        return createdTemplate.Name;
     }
 }
